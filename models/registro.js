48abcd269a2f77ae7b14b1196d330f3a65d909ab
Jhonatan Steven García Gómez <102763345+JHONATANGARCIAXD@users.noreply.github.com>
2025-06-28 22:13:48 -0500

    Revert "Primer commit"

--- models/registro.js
+++
@@ -1,8 +0,0 @@
-import mongoose from "mongoose";
-const registrochema =  new mongoose.Schema({
-    nombre:{type:String,},
-    correo:{type:String,},
-    contraseña:{type:String,},
-    confirmcontraseña:{type:String,},
-})
-export default mongoose.model("registro",registrochema)
--- routers/rutas.js
+++
@@ -1,5 +0,0 @@
-import { Router } from "express";
-import registroCliente from "../controllers/registro.js"
-const router= Router();
-router.post("/registro",registroCliente)
-export default router
